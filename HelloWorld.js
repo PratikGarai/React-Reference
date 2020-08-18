dc196e932466d87af1fad94f165e3ec05fa783f6
PratikGarai <pratikgarai0208@gmail.com>
2020-08-18 08:27:29 +0530

    Got the code right finally

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,4 +1,1 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-ReactDOM.render(<h2>Hello World!</h2>, document.getElementById("root"))
+ReactDOM.render(<h2>Hello World!</h2>, document.getElementById('root'));

a78aadb9884d37f358cf03d44663211bbea57be7
PratikGarai <pratikgarai0208@gmail.com>
2020-08-19 23:09:28 +0530

    Changed list to callable component

--- Basic/ListRendering.js
+++ Basic/ListRendering.js
@@ -1,1 +1,11 @@
-ReactDOM.render(<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>, document.getElementById("root"));
+function MyList(){
+	return (
+		<ul>
+			<li>Item 1</li>
+			<li>Item 2</li>
+			<li>Item 3</li>
+		</ul>
+	);
+}
+
+ReactDOM.render(<MyList />, document.getElementById("root"));

6837df5e01705edb68ef07dda9e2847d78669cc7
PratikGarai <pratikgarai0208@gmail.com>
2020-08-19 23:42:25 +0530

    Created the importing example

---
+++ Basic/Import_Example/Main.js
@@ -0,0 +1,3 @@
+import MyInfo from "./MyInfo.js"
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
---
+++ Basic/Import_Example/MyInfo.js
@@ -0,0 +1,15 @@
+function MyInfo() {
+	return (
+		<div>
+			<h1>My Info</h1>
+			<p> I am a CSE student </p>
+			<ul>
+				<li> Python </li>
+				<li> Java </li>
+				<li> C++ </li>
+			</ul>
+		</div>
+	);
+};
+
+export default MyInfo

4251d9d7a06587c49536fd5b8ae1d596c502157c
PratikGarai <pratikgarai0208@gmail.com>
2020-08-21 01:16:47 +0530

    Finally! Importing works !  :D

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Footer()
+{
+	return (
+		<footer>
+			<h3> This is a footer </h3>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header()
+{
+	return (
+      		<header className="App-header">
+	  		<h2>Header Here</h2>
+      		</header>
+	);
+}
+
+export default Header;

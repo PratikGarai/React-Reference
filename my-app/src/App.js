ae71495d5883176928f21c5a96a98b101e3df2cb
PratikGarai <pratikgarai0208@gmail.com>
2020-08-23 22:16:43 +0530

    Added styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
 import CheckList from './components/CheckList.js'
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react"
+import './Header.css'
 
 function Header()
 {
 	return (
-      		<header className="App-header">
-	  		<h2>Header Here</h2>
+      		<header className="header-full">
+	  		<h2>Header Heading</h2>
+			<nav className="header-nav">
+				<ul className = "header-nav__list">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+			</nav>
       		</header>
 	);
 }

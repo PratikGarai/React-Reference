183bd45a75c81d76c7f8be093ba56d3afc60f34d
PratikGarai <pratikgarai0208@gmail.com>
2020-09-04 14:35:08 +0530

    Fetcher example done

--- my-app/src/class-based/FetcherApp.js
+++ my-app/src/class-based/FetcherApp.js
@@ -10,6 +10,16 @@ class FetcherApp extends React.Component
 		}
 	}
 
+	componentDidMount(){
+		fetch("https://swapi.dev/api/people/1")
+		.then(response => response.json())
+		.then(data =>{
+			this.setState({
+				character : data
+			});
+		});
+	}
+
 	render(){
 		return(
 			<h1>{this.state.character.name?this.state.character.name:"No Data"}</h1>

60b794b118f5bda429678f90af53545a1046ac2b
PratikGarai <pratikgarai0208@gmail.com>
2020-09-11 08:37:51 +0530

    Added some stuff

--- my-app/src/class-based/LoginForm.js
+++ my-app/src/class-based/LoginForm.js
@@ -5,16 +5,34 @@ class LoginForm extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			name:"",
+			username:"",
 			password:"",
 			age:"",
 			gender:""
 		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event)
+	{
+		const {name, value} = event.target;
+		this.setState({
+			[name] : value
+		});
 	}
 
 	render(){
 		return(
 			<div className="main-div">
+			<form>
+				<input 
+					type="text"
+					name="username"
+					value={this.state.name}
+					placeholder = "Your Name Here"
+					onChange = {this.handleChange}
+				/>
+			</form>
 			</div>
 		);
 	}

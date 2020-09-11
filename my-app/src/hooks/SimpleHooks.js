63bd7d4d143c509cf7c353d111078b780effc044
PratikGarai <pratikgarai0208@gmail.com>
2020-09-11 14:14:41 +0530

    useState example added

--- my-app/src/hooks/SimpleHooks.js
+++ my-app/src/hooks/SimpleHooks.js
@@ -5,13 +5,35 @@ function SimpleHooks(props)
 	const [counter , setCounter] = useState(0);
 
 	const styles = {
-		backgroundColor : "cyan",
 		display : "inline-block",
 		fontSize : 30
 	}
+
+	const bstyles = {
+		height : 50,
+		borderRadius : 5,
+		fontSize : 30,
+		marginLeft : 20,
+		marginRight : 20
+	}
+
+	function handler(event){
+		const nm = event.target.name;
+		if(nm=="up") 	setCounter(counter+1);
+		else 	setCounter(counter-1);
+	}
+
 	return(
-		<div style={styles}>
-			Hello
+		<div>
+			<button name="up" style={bstyles} onClick={handler}>
+				UP
+			</button>
+			<div style={styles}>
+				{counter}
+			</div>
+			<button name="down" style={bstyles} onClick={handler}>
+				DOWN
+			</button>
 		</div>
 	);
 }

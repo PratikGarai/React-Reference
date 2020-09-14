ed5e2e1ba4e760d63e064cb848a4e5daee960d4e
PratikGarai <pratikgarai0208@gmail.com>
2020-09-14 10:31:52 +0530

    Completed playing around with static router stuff

--- my-app/src/router/staticRouteExample.js
+++ my-app/src/router/staticRouteExample.js
@@ -7,23 +7,37 @@ import { BrowserRouter as Router,
 function ExampleApp(props)
 {
 	return(
-		<Router>
-			<Link to ='/page1'>Page1</Link><br />
-			<Link to ='/page2'>Page2</Link>
-			<Switch>
-				<Route path='/page1'>
-					<PageApp1 />
-				</Route>
-				<Route path='/page2'>
-					<PageApp2 />
-				</Route>
-			</Switch>
-		</Router>
+		<div>
+			<Router>
+				<Link to ='/page1'>Page1</Link><br />
+				<Link to ='/page2'>Page2</Link>
+				<Switch>
+					<Route path='/page1'>
+						<PageApp1 />
+					</Route>
+					<Route path='/page2'>
+						<PageApp2 />
+					</Route>
+					<Route path='/'>
+						<PageApp0 />
+					</Route>
+				</Switch>
+			</Router>
+			<p>
+				This is part of main app.
+			</p>
+		</div>
 	);
 }
 
 export default ExampleApp;
 
+function PageApp0(props)
+{
+	return(
+		<h2> This is part of  main page using Route </h2>
+	);
+}
 function PageApp1( props )
 {
 	return(

e31604008d00607ad8a2b6ea26a1cc70b7661436
PratikGarai <pratikgarai0208@gmail.com>
2020-09-14 11:38:33 +0530

    Completed example of nested router

--- my-app/src/router/nestedRoutes.js
+++ my-app/src/router/nestedRoutes.js
@@ -48,13 +48,33 @@ function AboutApp(props)
 function TopicsApp(props)
 {
 	let match = useRouteMatch();
-	console.log(match);
 	return(
 		<div>
 			<h2> Welcome to the topics Page ! </h2>
 			<p> Here is a list of topics </p>
 			<Router>
+				<Link to = {match.url+"/link1"}>Link 1</Link><br />
+				<Link to = {match.url+"/link2"}>Link 2</Link><br />
+				<Link to = {match.url+"/link3"}>Link 3</Link><br />
+				<Switch>
+					<Route path={match.url+"/link1"}>
+						<TemplateTopic number="1" />
+					</Route>
+					<Route path={match.url+"/link2"}>
+						<TemplateTopic number="2" />
+					</Route>
+					<Route path={match.url+"/link3"}>
+						<TemplateTopic number="3" />
+					</Route>
+				</Switch>
 			</Router>
 		</div>
+	);
+}
+
+function TemplateTopic(props)
+{
+	return(
+	<h3> This is topic number {props.number}!</h3> 
 	);
 }

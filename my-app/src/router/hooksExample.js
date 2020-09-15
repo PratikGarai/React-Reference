05e4f8bf6acb013a5717396de94c3f906aae9568
PratikGarai <pratikgarai0208@gmail.com>
2020-09-15 06:18:31 +0530

    Added the basic stuff

--- my-app/src/router/hooksExample.js
+++ my-app/src/router/hooksExample.js
@@ -8,8 +8,41 @@ import { BrowserRouter as Router,
 function ExampleApp(props)
 {
 	return(
-		<h2>Hello</h2>
+		<Router>
+			<Link to = "/">Home</Link><br />
+			<Link to = "/somepage">Some Page</Link><br />
+			<Link to = "/about">About Page</Link><br />
+			<Link to = "/listpage">List Page</Link><br />
+			<Switch>
+				<Route exact path ="/">
+					<HomeComponent pageName="Home"/>
+				</Route>
+				<Route path ="/somepage">
+					<HomeComponent pageName="random"/>
+				</Route>
+				<Route path ="/about">
+					<HomeComponent pageName="About"/>
+				</Route>
+				<Route path ="">
+					<ListComponent /> 
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
 export default ExampleApp;
+
+function HomeComponent(props)
+{
+	return(
+		<p> This is the {props.pageName} page. </p>
+	);
+}
+
+function ListComponent(props)
+{
+	return(
+		<p> The list page </p>
+	);
+}
